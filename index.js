421aaeb651f05515372960f22bc7dd016eb28f93
Gustavo01010001 <163767997+Gustavo01010001@users.noreply.github.com>
2025-01-30 20:56:17 -0300

    ajuste

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".icons");
+    const navbar = document.querySelector(".navbar");
+
+    menuIcon.addEventListener("click", function () {
+        navbar.classList.toggle("active");
+    });
+});
